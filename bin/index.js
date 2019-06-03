287374f2dd5710e9c11c6c92ec0fe8c85fce83ed
Malaj Alexandre <alexandre.malaj@gmail.com>
2019-06-03 21:51:32 +0200

    feat: map with know deps to mutualise all repo

--- bin/index.js
+++ bin/index.js
@@ -12,7 +12,45 @@ const clipboardy = require("clipboardy");
 
 // CONSTANTS
 const PREFIX_VERSION = new Set(["^", "~", "*", ">"]);
+const KNOW_DEPS = new Map([
+    // [Key, [Refactoring, Severity Risk, Usage]]
+    ["@slimio/addon", ["Minor", "Low", "Addon container"]],
+    ["@slimio/addon-factory", ["Minor", "Low", "Addon generator"]],
+    ["@slimio/alert", ["Minor", "Low", "Create & manage addon's alerts"]],
+    ["@slimio/arg-parser", ["Minor", "Low", "Script argument parser"]],
+    ["@slimio/async-cli-spinner", ["Minor", "Low", "Multi async cli spinner"]],
+    ["@slimio/github", ["Minor", "Medium", "Github repositories downloader"]],
+    ["@slimio/is", ["Minor", "Low", "Type checker"]],
+    ["@slimio/lazy", ["Minor", "Low", "Setup lazy props on Objects"]],
+    ["@slimio/Manifest", ["Minor", "Low", "Load & manage SlimIO Manifest"]],
+    ["@slimio/metrics", ["Minor", "Low", "Create & manage addon's metrics"]],
+    ["@slimio/npm-registry", ["Minor", "Low", "GET data from NPM registry"]],
+    ["@slimio/pretty-json", ["Minor", "Low", "Stdout beautified JSON"]],
+    ["@slimio/safe-emitter", ["Minor", "Medium", "Node.js Safe Emitter"]],
+    ["@slimio/tcp-sdk", ["Minor", "Low", "SlimIO TCP-Sdk"]],
+    ["@slimio/timer", ["Minor", "Low", "Driftless interval"]],
+    ["@slimio/unzipper", ["Minor", "High", "Modern yauzl wrapper used to unzip .zip archive in extract() method."]],
+    ["@slimio/units", ["Minor", "Low", "Bunch of units for metrics"]],
+    ["@slimio/utils", ["Minor", "Low", "Bunch of useful functions"]],
 
+    // eslint-disable-next-line max-len
+    ["make-promise-safe", ["⚠️Major", "Low", "Force Node.js [DEP00018](https://nodejs.org/dist/latest-v8.x/docs/api/deprecations.html#deprecations_dep0018_unhandled_promise_rejections)"]],
+    ["dotenv", ["Minor", "Low", "Env file"]],
+
+    ["polka", ["⚠️Major", "Low", "Native HTTP server"]],
+    ["@polka/send-type", ["Minor", "Low", "HTTP response helper"]],
+
+    ["lodash.get", ["Minor", "Low", "Get a value"]],
+    ["lodash.set", ["Minor", "Low", "Set a value"]],
+    ["lodash.clonedeep", ["Minor", "Low", "Clone deep Objects"]],
+
+    ["repos", ["⚠️Major", "Low", "Get all GIT repositories"]],
+    ["kleur", ["Minor", "Low", "TTY color"]],
+    ["httpie", ["Minor", "Low", "	HTTP request"]],
+    ["zen-observable", ["Minor", "Low", "Observable Implementation"]],
+    ["body-parser", ["⚠️Major", "High", "Body Parser"]],
+    ["semver", ["⚠️Major", "Low", "SemVer validation"]]
+]);
 
 const argv = parseArg([
     argDefinition("-c --clipboard", "Copy directly to clipboard")
